9c25663ab3e912579816ba2fee202df8da763600
JiaRuibin <ruibin231@gmail.com>
2017-12-03 20:47:43 +0800

    add webpack and vue

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>资产推介平台</p>
+  </div>
+</template>
+<script>
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+    },
+    methods: {
+    }
+  }
+</script>
