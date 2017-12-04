df4f7d07247a9f425b795ffe1895da4b5770f034
JiaRuibin <jiaruibin@qfpay.com>
2017-12-04 15:31:04 +0800

    add models

---
+++ grocery/properties/models.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=16, unique=True,
+                            verbose_name=u'资产类型')
+
+    class Mate:
+        verbose_name = verbose_name_plural = u'资产分类'
+
+    def __unicode__(self):
+        return self.name
+
+
+class Assets(models.Model):
+    category = models.ForeignKey(Category, verbose_name=u'分类')
+    title = models.CharField(max_length=32, verbose_name=u'资产名称')
+    # 说明
+    instruction = models.TextField(verbose_name=u'资产说明')
+    # 配套信息
+    parms = models.TextField(verbose_name=u'配套信息')
+
+    bond_institution = models.CharField(max_length=32, verbose_name=u'债权机构')
+    obligor = models.CharField(max_length=8, verbose_name=u'债务人')
+    guarantee = models.CharField(max_length=8, verbose_name=u'保证人')
+    mortgagor = models.CharField(max_length=8, verbose_name=u'抵押人')
+    # 资产亮点
+    spot = models.TextField(verbose_name=u'资产亮点')
+    # 受理公示事项
+    contacts = models.CharField(max_length=8, verbose_name=u'联系人')
+    c_phone = models.CharField(max_length=16, verbose_name=u'联系人电话')
+    fax = models.CharField(max_length=16, verbose_name=u'传真')
+    p_address = models.CharField(max_length=64, verbose_name=u'通讯地址')
+    # 交易对象及声明
+    transaction = models.TextField(verbose_name=u'交易对象')
+    statement = models.TextField(verbose_name=u'声明')
+
+    pub_time = models.DateTimeField(auto_now_add=True, verbose_name=u'发布时间')
+
+    class Mate:
+        verbose_name = verbose_name_plural = u'资产信息'
+
+    def __unicode__(self):
+        return self.title
+
+
+
+class AssetsImg(models.Model):
+    assets = models.ForeignKey(Assets, verbose_name=u'资产')
+    large = models.ImageField(upload_to='%Y/%m/%d/')
+    middle = models.ImageField(upload_to='%Y/%m/%d/')
+    small = models.ImageField(upload_to='%Y/%m/%d/')
+
+    class Mate:
+        verbose_name = verbose_name_plural = u'资产图片'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, verbose_name=u'评论人')
+    asserts = models.ForeignKey(Assets, verbose_name=u'资产')
+    content = models.TextField(verbose_name=u'评论内容')
+    c_time = models.DateTimeField(auto_now_add=True, verbose_name=u'评论时间')
+
+    class Mate:
+        verbose_name = verbose_name_plural = u'资产评论'
+
+    def __unicode__(self):
+        return u'%s...' % self.content[:10]
--- grocery/wishlists/models.py
+++ grocery/wishlists/models.py
@@ -2,5 +2,32 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from grocery.properties.models import Assets
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    assets = models.ForeignKey(Assets, verbose_name=u'资产')
+    author = models.ForeignKey(User, verbose_name=u'用户')
+    title = models.CharField(max_length=32, verbose_name=u'资产标题')
+    time = models.DateTimeField(auto_now_add=True, verbose_name=u'收藏时间')
+
+    class Mate:
+        verbose_name = verbose_name_plural = u'收藏'
+
+    def __unicode__(self):
+        return self.title
+
+
+class Price(models.Model):
+    author = models.ForeignKey(User, verbose_name=u'出价人')
+    assets = models.ForeignKey(Assets, verbose_name=u'报价资产')
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True, verbose_name=u'收藏时间')
+
+    class Mate:
+        verbose_name = verbose_name_plural = u'报价'
+
+    def __unicode__(self):
+        return self.assets.title
+
